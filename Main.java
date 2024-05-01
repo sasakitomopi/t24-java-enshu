35c427f1a2f1e94f736d9b06e085de3413fc2a63
sasakitomopi <takatobi0326@icloud.com>
2024-05-02 01:38:31 +0900

    feat: 演習問題1の作成

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[]args){
+    while(true){
+      System.out.println("1~100の整数を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if(num % 7 == 0){
+        break;
+      }
+    }
+    System.out.println("処理を終了しました。");
+  }
+}
